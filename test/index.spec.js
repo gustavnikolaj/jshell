0ff141d275e57c6026e8c103a045d6bfb2b7ad24
Gustav Nikolaj Olsen <gno@one.com>
2014-11-04 16:54:23 +0100

    initial prototype commit
    todo:
    - return promise if no arguments given to lines
    - pipe to a node.js writeable stream
    - implement .buffer as .lines
    - implement .toString as .lines

---
+++ test/index.spec.js
@@ -0,0 +1,44 @@
+/*global describe, it*/
+var expect = require('unexpected');
+var jshell = require('../lib/index');
+
+describe('jshell', function () {
+    it('should be a function', function () {
+        expect(jshell, 'to be a function');
+    });
+    it('should return an object with a lines method', function () {
+        expect(jshell('echo foo'), 'to satisfy', {
+            lines: expect.it('to be a function')
+        });
+    });
+    it('should return an object with a pipe method', function () {
+        expect(jshell('echo foo'), 'to satisfy', {
+            pipe: expect.it('to be a function')
+        });
+    });
+    describe('lines', function () {
+        it('should pass the output lines of the jshell command to the callback', function (done) {
+            jshell('echo', 'foo').lines(function (err, data) {
+                expect(err, 'to be null');
+                expect(data, 'to equal', ['foo']);
+                done();
+            });
+        });
+    });
+    describe('pipe', function () {
+        it('should pipe output to grep', function (done) {
+            jshell('echo', 'foo').pipe('grep', 'foo').lines(function (err, data) {
+                expect(err, 'to be null');
+                expect(data, 'to equal', ['foo']);
+                done();
+            });
+        });
+        it('should pipe output to grep with args', function (done) {
+            jshell('echo', 'foo').pipe('grep', '-v', 'foo').lines(function (err, data) {
+                expect(err, 'to be null');
+                expect(data, 'to equal', []);
+                done();
+            });
+        });
+    });
+});
