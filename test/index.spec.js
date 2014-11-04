f6218c44729c025e7abf43ceaac5d287d7473ef1
Sune Simonsen <sune@we-knowhow.dk>
2014-11-04 21:24:47 +0100

    Fixes #3

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,16 +7,14 @@ describe('jshell', function () {
         expect(jshell, 'to be a function');
     });
     it('should return an object with a lines method', function () {
-        expect(jshell('echo', 'foo').lines, 'to be a function');
-        // expect(jshell('echo', 'foo'), 'to satisfy', {
-        //     lines: expect.it('to be a function')
-        // });
+        expect(jshell('echo', 'foo'), 'to satisfy', {
+            lines: expect.it('to be a function')
+        });
     });
     it('should return an object with a pipe method', function () {
-        expect(jshell('echo', 'foo').pipe, 'to be a function');
-        // expect(jshell('echo', 'foo'), 'to satisfy', {
-        //     pipe: expect.it('to be a function')
-        // });
+        expect(jshell('echo', 'foo'), 'to satisfy', {
+            pipe: expect.it('to be a function')
+        });
     });
     describe('toBuffer', function () {
         it('should pass the output buffer of the jshell command to the callback', function (done) {
@@ -28,11 +26,19 @@ describe('jshell', function () {
         });
     });
     describe('toString', function () {
-        it('should pass the output string of the jshell command to the callback', function (done) {
-            jshell('echo', 'foo').toString(function (err, data) {
-                expect(err, 'to be null');
-                expect(data, 'to equal', 'foo\n');
-                done();
+        describe('given no arguments', function () {
+            it('should return a string representing the command pipeline', function () {
+                expect(jshell('echo', 'foo').pipe('grep', 'foo').toString(), 'to equal', 'echo foo | grep foo');
+            });
+        });
+
+        describe('given a callback', function () {
+            it('should pass the output string of the jshell command to the callback', function (done) {
+                jshell('echo', 'foo').toString(function (err, data) {
+                    expect(err, 'to be null');
+                    expect(data, 'to equal', 'foo\n');
+                    done();
+                });
             });
         });
     });
