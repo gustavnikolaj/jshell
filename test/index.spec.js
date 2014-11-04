787470e672b42482ea2f194a570973938f18e159
Sune Simonsen <sune@we-knowhow.dk>
2014-11-04 23:05:59 +0100

    Support piping to a writable stream
    Fixes #2

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,25 @@
 /*global describe, it*/
 var expect = require('unexpected');
 var jshell = require('../lib/index');
+var stream = require('stream');
+var util = require('util');
+
+function WritableMemoryStream(options) {
+    if (!(this instanceof WritableMemoryStream)) {
+        return new WritableMemoryStream(options);
+    }
+    stream.Writable.call(this, options);
+    this.buffer = new Buffer('');
+}
+util.inherits(WritableMemoryStream, stream.Writable);
+
+WritableMemoryStream.prototype._write = function (chunk, enc, cb) {
+    var buffer = (Buffer.isBuffer(chunk)) ?
+        chunk : new Buffer(chunk, enc);
+
+    this.buffer = Buffer.concat([this.buffer, buffer]);
+    cb();
+};
 
 describe('jshell', function () {
     it('should be a function', function () {
@@ -63,6 +82,14 @@ describe('jshell', function () {
             jshell('echo', 'foo').pipe('grep', '-v', 'foo').lines(function (err, data) {
                 expect(err, 'to be null');
                 expect(data, 'to equal', []);
+                done();
+            });
+        });
+        it('should pipe output to a writable stream', function (done) {
+            var writableStream = new WritableMemoryStream();
+            jshell('echo', 'foo').pipe('grep', 'foo').pipe(writableStream);
+            writableStream.on('finish', function () {
+                expect(writableStream.buffer.toString(), 'to equal', 'foo\n');
                 done();
             });
         });
